e15d046de1571e0d6d6f5c8f0b1b1bdd06f8ca79
mark.j.pernot <mark.j.pernot.ctr@mail.mil>
2019-09-04 16:24:07 +0100

    Update test/unit/mysql_perf/mysql_stat_run.py

--- test/unit/mysql_perf/mysql_stat_run.py
+++ test/unit/mysql_perf/mysql_stat_run.py
@@ -110,13 +110,11 @@ class Server(object):
         """
 
         self.name = "ServerName"
-        self.cur_mem_mb = "cur_mem_mb"
-        self.max_mem_mb = "max_mem_mb"
-        self.prct_mem = "prct_mem"
-        self.days_up = "days_up"
-        self.cur_conn = "cur_conn"
+        self.binlog_disk = "binlog_disk"
+        self.uptime_flush = "uptime_flush"
         self.max_conn = "max_conn"
-        self.prct_conn = "prct_conn"
+        self.uptime = "uptime"
+        self.cur_conn = "cur_conn"
 
     def upd_srv_stat(self):
 
@@ -151,11 +149,12 @@ class UnitTest(unittest.TestCase):
 
     Methods:
         setUp -> Initialize testing environment.
+        test_error_handling -> Test error handling.
         test_mail -> Test with emailing out.
         test_file -> Test with writing to file.
         test_mongo -> Test with mongo connection.
         test_default -> Test with default settings.
-        test_json -> Test with in JSON format.
+        test_perf_list -> Test with perf_list populated.
 
     """
 
@@ -173,6 +172,25 @@ class UnitTest(unittest.TestCase):
         self.mail = Mail()
         self.args_array = {"-j": True}
         self.args_array2 = {}
+        self.perf_list = ["uptime_flush", "binlog_disk", "cur_conn", "uptime",
+                          "max_conn"]
+
+    @mock.patch("mysql_perf.gen_libs.print_dict")
+    def test_error_handling(self, mock_print):
+
+        """Function:  test_error_handling
+
+        Description:  Test error handling.
+
+        Arguments:
+
+        """
+
+        mock_print.return_value = (True, "Error Message")
+
+        with gen_libs.no_std_out():
+            self.assertFalse(mysql_perf.mysql_stat_run(self.server,
+                perf_list=self.perf_list))
 
     @unittest.skip("not yet implemented")
     @mock.patch("mysql_perf.mongo_libs.ins_doc")
@@ -215,9 +233,9 @@ class UnitTest(unittest.TestCase):
     @unittest.skip("not yet implemented")
     @mock.patch("mysql_perf.mongo_libs.ins_doc")
     @mock.patch("mysql_perf.gen_libs.write_file")
-    def test_mongo(self, mock_write, mock_mongo):
+    def test_mongo2(self, mock_write, mock_mongo):
 
-        """Function:  test_mongo
+        """Function:  test_mongo2
 
         Description:  Test with mongo connection.
 
@@ -231,6 +249,25 @@ class UnitTest(unittest.TestCase):
         self.assertFalse(mysql_perf.mysql_stat_run(self.server,
                                                    db_tbl="db:tbl",
                                                    class_cfg="Cfg"))
+
+    @mock.patch("mysql_perf.mongo_libs.json_2_out")
+    @mock.patch("mysql_perf.gen_libs.print_dict")
+    def test_mongo(self, mock_print, mock_mongo):
+
+        """Function:  test_mongo
+
+        Description:  Test with mongo connection.
+
+        Arguments:
+
+        """
+
+        mock_print.return_value = (False, None)
+        mock_mongo.return_value = True
+
+        self.assertFalse(mysql_perf.mysql_stat_run(self.server,
+                                                   db_tbl="db:tbl",
+                                                   perf_list=self.perf_list))
 
     @mock.patch("mysql_perf.gen_libs.print_dict")
     def test_default(self, mock_print):
@@ -247,14 +284,30 @@ class UnitTest(unittest.TestCase):
 
         self.assertFalse(mysql_perf.mysql_stat_run(self.server))
 
+    @mock.patch("mysql_perf.gen_libs.print_dict")
+    def test_perf_list(self, mock_print):
+
+        """Function:  test_perf_list
+
+        Description:  Test with perf_list populated.
+
+        Arguments:
+
+        """
+
+        mock_print.return_value = (False, None)
+
+        self.assertFalse(mysql_perf.mysql_stat_run(self.server,
+                                                   perf_list=self.perf_list))
+
     @unittest.skip("not yet implemented")
     @mock.patch("mysql_perf.mongo_libs.ins_doc")
     @mock.patch("mysql_perf.gen_libs.write_file")
-    def test_json(self, mock_write, mock_mongo):
+    def test_perf_list2(self, mock_write, mock_mongo):
 
-        """Function:  test_json
+        """Function:  test_perf_list2
 
-        Description:  Test with in JSON format.
+        Description:  Test with perf_list populated.
 
         Arguments:
 
