df269514c214a667dcde2dbe46996c08746a56db
mark.j.pernot <mark.j.pernot.ctr@mail.mil>
2020-07-31 10:47:30 +0100

    Updated test/unit/mysql_perf/unit_test_run.sh v2.3.0

--- test/unit/mysql_perf/unit_test_run.sh
+++ test/unit/mysql_perf/unit_test_run.sh
@@ -7,6 +7,7 @@
 echo ""
 echo "Unit testing..."
 test/unit/mysql_perf/help_message.py
+test/unit/mysql_perf/is_pos_int.py
 test/unit/mysql_perf/main.py
 test/unit/mysql_perf/mysql_stat.py
 test/unit/mysql_perf/mysql_stat_run.py
