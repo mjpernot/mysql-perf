2d57a79667968d64f0b3f0c08be879c26b4156e5
Mark Pernot <mark.pernot@gmail.com>
2022-11-29 10:01:20 +0000

    Update unit test modules v2.4.2

--- test/unit/mysql_perf/convert_dict.py
+++ test/unit/mysql_perf/convert_dict.py
@@ -67,12 +67,16 @@ class UnitTest(unittest.TestCase):
         self.results = ""
         self.results2 = "key1:  value1\n"
         self.results3 = "key2:  value2\nkey1:  value1\n"
+        self.results3a = "key1:  value1\nkey2:  value2\n"
         self.results4 = "key1:\n    key1a:  value1a\n"
         self.results5 = "key2:  value2\nkey1:\n    key1a:  value1a\n"
+        self.results5a = "key1:\n    key1a:  value1a\nkey2:  value2\n"
         self.sub = "key1:  value1\n"
         self.sub2 = "key3:\n    key3A:\n        key3AA:  value3AA\n"
         self.sub3 = "key2:\n    key2B:  value2B\n    key2A:  value2A\n"
+        self.sub3a = "key2:\n    key2A:  value2A\n    key2B:  value2B\n"
         self.results6 = self.sub2 + self.sub3 + self.sub
+        self.results6a = self.sub + self.sub3a + self.sub2
 
     def test_nested_dicts(self):
 
@@ -86,7 +90,13 @@ class UnitTest(unittest.TestCase):
 
         mysql_perf.convert_dict(self.data6, self.mail)
 
-        self.assertEqual(self.mail.msg, self.results6)
+        if sys.version_info < (3, 0):
+            results = self.results6
+
+        else:
+            results = self.results6a
+
+        self.assertEqual(self.mail.msg, results)
 
     def test_multi_key_lvl(self):
 
@@ -100,7 +110,13 @@ class UnitTest(unittest.TestCase):
 
         mysql_perf.convert_dict(self.data5, self.mail)
 
-        self.assertEqual(self.mail.msg, self.results5)
+        if sys.version_info < (3, 0):
+            results = self.results5
+
+        else:
+            results = self.results5a
+
+        self.assertEqual(self.mail.msg, results)
 
     def test_multi_lvl(self):
 
@@ -128,7 +144,13 @@ class UnitTest(unittest.TestCase):
 
         mysql_perf.convert_dict(self.data3, self.mail)
 
-        self.assertEqual(self.mail.msg, self.results3)
+        if sys.version_info < (3, 0):
+            results = self.results3
+
+        else:
+            results = self.results3a
+
+        self.assertEqual(self.mail.msg, results)
 
     def test_one_item(self):
 
